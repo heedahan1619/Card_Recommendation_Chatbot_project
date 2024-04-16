91f36ae4e88e015bbeeab1425247ca2ee644e21e
heedahan1619 <heedahan1619@gmail.com>
2024-04-17 00:26:27 +0900

    카드사 json 파일 로드

--- cards/cards/spiders/card_spider.py
+++ cards/cards/spiders/card_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import requests
 
 class CardGorillaSpider(scrapy.Spider):
     """카드고릴라 사이트 spider"""
@@ -7,6 +8,7 @@ class CardGorillaSpider(scrapy.Spider):
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
     }
     home_url = "https://www.card-gorilla.com/home"
+    json_url = "https://api.card-gorilla.com:8080/v1/"
 
     def start_requests(self):
         """크롤러가 시작하면서 실행하는 메소드"""
@@ -17,6 +19,18 @@ class CardGorillaSpider(scrapy.Spider):
             scrapy.Request(
                 url=self.home_url
                 ,headers=self.headers
+                ,callback=self.parse_company
             )
         ]
     
+    def parse_company(self, response):
+        """카드사 json 파일 불러오기"""
+
+        print("카드사 json 로드 성공")
+
+        card_corps = f"{self.json_url}card_corps"
+
+        res = requests.get(card_corps, headers=self.headers)
+        data = res.json()
+
+        print(data)
