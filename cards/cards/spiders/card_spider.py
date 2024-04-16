a7b2bef0c4eb1170f2e8c9f7201a999b5c2ea3ac
heedahan1619 <heedahan1619@gmail.com>
2024-04-17 00:41:08 +0900

    카드사 딕셔너리 생성

--- cards/cards/spiders/card_spider.py
+++ cards/cards/spiders/card_spider.py
@@ -7,8 +7,11 @@ class CardGorillaSpider(scrapy.Spider):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
     }
+
     home_url = "https://www.card-gorilla.com/home"
     json_url = "https://api.card-gorilla.com:8080/v1/"
+
+    company_dict = {} # 카드사 딕셔너리 - {카드사명:[카드사 인덱스, 카드사 로고 이미지 url]}
 
     def start_requests(self):
         """크롤러가 시작하면서 실행하는 메소드"""
@@ -26,11 +29,20 @@ class CardGorillaSpider(scrapy.Spider):
     def parse_company(self, response):
         """카드사 json 파일 불러오기"""
 
-        print("카드사 json 로드 성공")
+        print("카드사 json 파일 로드 완료")
 
         card_corps = f"{self.json_url}card_corps"
 
         res = requests.get(card_corps, headers=self.headers)
         data = res.json()
 
-        print(data)
+        for i in range(len(data)):
+            corps = data[i]
+            company_idx = corps['idx'] # 카드사 인덱스
+            company_name = corps['name'] # 카드사명
+            company_logo_img_url = corps['logo_img']['url'] # 카드사 로고 이미지 url
+            
+            self.company_dict[company_name] = [company_idx, company_logo_img_url]
+            
+        print(self.company_dict)
+
