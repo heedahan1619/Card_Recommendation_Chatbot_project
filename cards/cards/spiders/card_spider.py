3d645b428228c2c54c269f35aafb7b9eedef7dc3
heedahan1619 <heedahan1619@gmail.com>
2024-04-18 00:20:57 +0900

    카드사 인덱스 리스트 생성

--- cards/cards/spiders/card_spider.py
+++ cards/cards/spiders/card_spider.py
@@ -7,9 +7,11 @@ class CardGorillaSpider(scrapy.Spider):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
     }
+    page_request = 10
 
     home_url = "https://www.card-gorilla.com/home"
     json_url = "https://api.card-gorilla.com:8080/v1/"
+    card_url = "https://api.card-gorilla.com:8080/v1/cards/search?"
 
     company_dict = {} # 카드사 딕셔너리 - {카드사명:[카드사 인덱스, 카드사 로고 이미지 url]}
 
@@ -20,29 +22,35 @@ class CardGorillaSpider(scrapy.Spider):
 
         return [
             scrapy.Request(
-                url=self.home_url
+                url=f"{self.json_url}card_corps"
                 ,headers=self.headers
-                ,callback=self.parse_company
+                ,callback=self.parse_company_data
             )
         ]
     
-    def parse_company(self, response):
-        """카드사 json 파일 불러오기"""
+    def parse_company_data(self, response):
+        """카드사 json 데이터 불러오기"""
 
-        print("카드사 json 파일 로드 완료")
+        print("카드사 json 데이터 로드 완료")
 
-        card_corps = f"{self.json_url}card_corps"
-
-        res = requests.get(card_corps, headers=self.headers)
+        res = requests.get(response.url, headers=self.headers)
         data = res.json()
 
+        company_idx_list = []
         for i in range(len(data)):
             corps = data[i]
             company_idx = corps['idx'] # 카드사 인덱스
+            company_idx_list.append(company_idx)
             company_name = corps['name'] # 카드사명
+            company_name_eng = corps['name_eng'] # 영문 카드사명
+            if " " in company_name_eng:
+                company_name_eng = company_name_eng.split(" ")[0]
+            if "Card" in company_name_eng:
+                company_name_eng = company_name_eng.replace("Card", "")
+            company_name_eng = company_name_eng.lower()
+        
             company_logo_img_url = corps['logo_img']['url'] # 카드사 로고 이미지 url
             
             self.company_dict[company_name] = [company_idx, company_logo_img_url]
-            
-        print(self.company_dict)
 
+        print(company_idx_list)
