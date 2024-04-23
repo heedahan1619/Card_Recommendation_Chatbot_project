e686d9e0408397f81aceb88c3294d0fa48bbd81e
heedahan <heedahan1619@gmail.com>
2024-04-24 06:12:19 +0900

    주요 혜택 상세 안내 html 문자 변환

--- cards/cards/spiders/card_spider.py
+++ cards/cards/spiders/card_spider.py
@@ -132,8 +132,8 @@ class CardGorillaSpider(scrapy.Spider):
                     card_pr_detail = re.sub('\\<br\\>', '\n', card_pr_detail)
 
                     annual_fee_detail = card['annual_fee_detail'].replace('&amp;', '&').replace('&trade;', '™') # 연회비 상세안내
-                    annual_fee_detail = re.sub('\\<br\\>', '\n', annual_fee_detail)
-                    annual_fee_detail = re.sub('\\<p.+\\>', '', annual_fee_detail)
+                    annual_fee_detail = re.sub('<br>', '\n', annual_fee_detail)
+                    annual_fee_detail = re.sub('<p(\\s\\S+)+>', '', annual_fee_detail)
 
                     brand_code_list = [] # 카드 브랜드 코드 리스트 생성
                     for brand in card['brand']:
@@ -149,9 +149,11 @@ class CardGorillaSpider(scrapy.Spider):
                         key_benefit_comment = key['comment'] # 주요 혜택 설명
                         key_benefit_title = key['title'] # 주요 혜택 타이틀
                         key_benefit_logo_img_url = key['cate']['logo_img']['url'] # 주요 혜택 로고 이미지 url
-                        key_benefit_info = key['info'] # 주요 혜택 상세안내
+                        key_benefit_info = key['info'].replace('&nbsp;', '\n').replace('<br>', '').replace('&ldquo;', '“').replace('&rdquo;', '”').replace('&quot;', '"').replace('&amp;', '&').replace('&middot;', '·').replace('&bull;', '•').replace('&trade;', '™').replace("&#39;", "'").replace('&gt;', '>').replace('&lsquo;', '‘').replace('&rsquo;', '’') # 주요 혜택 상세안내
+                        key_benefit_info = re.sub('<p(\\s\\S+)+>', '', key_benefit_info)
+                        # key_benefit_info = re.sub('(<p>-)', '\n-', key_benefit_info)
 
-                        print(f"{key_benefit_info}\n")
+                        print(f"{card_idx}\t{key_benefit_info}\n")
                     
 
                     # print(f"\n{card_type}")
