9a78a1a4116ec40957dd7cc2461ca9a467b23f9b
heedahan <heedahan1619@gmail.com>
2024-04-25 02:01:08 +0900

    Items 설정

--- cards/cards/items.py
+++ cards/cards/items.py
@@ -9,4 +9,11 @@ import scrapy
 class CardsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    card_img_url = scrapy.Field() # 카드 이미지 url
+    card_name = scrapy.Field() # 카드명
+    corp_name = scrapy.Field() # 카드사명
+    card_pr = scrapy.Field() # 카드 pr 문구
+    top_benefit_tags_list = scrapy.Field() # 상위 혜택 태그 리스트
+    annual_fee_basic = scrapy.Field() # 연회비 기본
+    pre_month_money = scrapy.Field() # 전월실적
+    only_online = scrapy.Field() # 온라인 발급 전용
