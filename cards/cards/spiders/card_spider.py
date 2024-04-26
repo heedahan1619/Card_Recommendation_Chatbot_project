b6b9c4f77177631b0faf27e114614872e17f4ddc
heedahan <heedahan1619@gmail.com>
2024-04-26 17:37:32 +0900

    페이지 처리 코드 수정

--- cards/cards/spiders/card_spider.py
+++ cards/cards/spiders/card_spider.py
@@ -51,41 +51,49 @@ class CardGorillaSpider(scrapy.Spider):
             company_logo_img_url = corps['logo_img']['url'] # 카드사 로고 이미지 url
             self.company_dict[company_idx] = [company_name, company_name_eng, company_logo_img_url]
 
-        return [
-            scrapy.Request(
+        for page in range(1, self.page_request+1):
+            yield scrapy.Request(
                 url=f"{self.json_url}cards/search?p={page}&perPage=30&corp={company_idx}"
                 ,headers=self.headers
                 ,callback=self.parse_card_data
             )
-            for page in range(1, self.page_request+1)
-        ]
 
     def parse_card_data(self, response):
         """카드 json 데이터 로드"""
+
+        print(f"response.url: {response.url}")
 
         for company_idx in company_idx_list:
 
             url = f"{response.url[:-2]}{company_idx}"
             res = requests.get(url, headers=self.headers)
             data = res.json()['data']
-
-            # print(f"url: {url}")
             
             if data != []:
                 for i in range(len(data)):
+
                     annual_fee_basic = data[i]['annual_fee_basic'].replace("[", "").replace("]", "") # 연회비 기본
+                    
                     card_type = data[i]['c_type_txt'] # 카드 타입
+                    
                     card_img_url = data[i]['card_img']['url'] # 카드 이미지 url
+                    
                     card_cate = f"{data[i]['cate_txt']}카드" # 카드 종류
+                    
                     card_idx = data[i]['cid'] # 카드 인덱스
+                    
                     corp_idx = data[i]['corp_idx'] # 카드사 인덱스
+                    
                     corp_name = data[i]['corp_txt'] # 카드사명
+                    
                     card_name = data[i]['name'] # 카드명
+                    
                     only_online = data[i]['only_online'] # 온라인발급 전용 카드
                     if only_online == True:
                         only_online = "온라인발급 전용 카드"
                     else:
                         only_online = ""
+                    
                     pre_month_money = str(data[i]['pre_month_money']) # 전월실적
                     if len(pre_month_money) == 6:
                         pre_month_money = f"전월실적 {pre_month_money[:2]}만원 이상"
@@ -93,11 +101,13 @@ class CardGorillaSpider(scrapy.Spider):
                         pre_month_money = f"전월실적 {pre_month_money[:3]}만원 이상"
                     else:
                         pre_month_money = "전월실적 없음"
+                    
                     is_discon = data[i]['is_discon'] # 발급 중단 카드 
                     if is_discon == True:
                         is_discon = "신규발급이 중단된 카드입니다."
                     else:
                         is_discon = ""
+
                     global search_benefit_dict
                     search_benefit_dict = {} # 검색 혜택 딕셔너리 - {검색 혜택 타이틀:[검색 혜택 라벨]}
                     search_benefit_title_list = [] # 검색 혜택 타이틀 리스트
@@ -109,8 +119,9 @@ class CardGorillaSpider(scrapy.Spider):
                             search_benefit_label = search_benefit_options['label'] # 검색 혜택 라벨
                             search_benefit_label_list.append(search_benefit_label)
                         search_benefit_dict[search_benefit_title] = search_benefit_label_list
+                    
                     global top_benefit_dict
-                    top_benefit_dict = {} # 상위 혜택 딕셔너리 - {상위 혜택 타이틀:상위 혜택 태그}
+                    top_benefit_dict = {} # 상위 혜택 딕셔너리 - {상위 혜택 태그:[상위 혜택 로고 이미지 url, 상위 혜택 타이틀]}
                     top_benefit_title_list = [] # 상위 혜택 타이틀 리스트
                     top_benefit_tags_list = [] # 상위 혜택 태그 리스트
                     for top_benefit in data[i]['top_benefit']:
@@ -118,7 +129,8 @@ class CardGorillaSpider(scrapy.Spider):
                         top_benefit_title_list.append(top_benefit_title)
                         top_benefit_tags = f"{top_benefit['tags'][0]} {top_benefit['tags'][1]} {top_benefit['tags'][2]}" # 상위 혜택 태그
                         top_benefit_tags_list.append(top_benefit_tags)
-                        top_benefit_dict[top_benefit_title] = top_benefit_tags
+                        top_benefit_logo_img_url = top_benefit['logo_img']['url']
+                        top_benefit_dict[top_benefit_tags] = [top_benefit_logo_img_url, top_benefit_title]
 
                     card_url = f"{self.json_url}cards/{card_idx}"
                     
@@ -154,6 +166,4 @@ class CardGorillaSpider(scrapy.Spider):
                         key_benefit_info = re.sub('(<p>)|&nbsp;', '\n', key_benefit_info)
                         key_benefit_dict[key_benefit_title] = [key_benefit_logo_img_url, key_benefit_comment, key_benefit_info]
 
-                    print(f"{card_idx}\t{key_benefit_dict}\n")
-                    
-
+                    request_m = card['request_m'] # 카드사 바로가기 url
