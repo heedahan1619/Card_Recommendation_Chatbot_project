83366c618ffc14db80eb7759f51528afefa5330f
heedahan <heedahan1619@gmail.com>
2024-05-09 14:19:08 +0900

    items 파일 수정

--- cards/cards/items.py
+++ cards/cards/items.py
@@ -13,7 +13,13 @@ class CardsItem(scrapy.Item):
     card_name = scrapy.Field() # 카드명
     corp_name = scrapy.Field() # 카드사명
     card_pr = scrapy.Field() # 카드 pr 문구
-    top_benefit_tags_list = scrapy.Field() # 상위 혜택 태그 리스트
+    top_benefit_list = scrapy.Field() # 상위 혜택 리스트
+    is_discon = scrapy.Field() # 신규 발급 중단
     annual_fee_basic = scrapy.Field() # 연회비 기본
     pre_month_money = scrapy.Field() # 전월실적
+    brand_list = scrapy.Field() # 카드 브랜드 리스트
     only_online = scrapy.Field() # 온라인 발급 전용
+    key_benefit_list = scrapy.Field() # 주요 혜택 리스트
+    awards_list = scrapy.Field() # 고릴라 어워즈
+    compare_card_list = scrapy.Field() # 많이 비교된 카드 - 최근 1개월
+    ranking_dict = scrapy.Field() # 카드사별 인기순위
